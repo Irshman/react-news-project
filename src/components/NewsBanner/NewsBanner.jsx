ac091c4205e37d3e792b2a9c07640ef656d683a9
o1afff <oafthree19@gmail.com>
2024-03-02 23:18:34 +0200

    Refactoring code filter

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,7 +1,7 @@
 import {formatTimeAgo} from "../../helpers/formatTimeAgo.js";
 import styles from './styles.module.css'
 import Image from "../Image/Image.jsx";
-import withSkeleton from "../../helpers/hocs/withSkeleton.jsx";
+
 const NewsBanner = ({ item }) => {
     return <div className={styles.banner}>
         <Image image={item?.image} />
@@ -10,6 +10,4 @@ const NewsBanner = ({ item }) => {
     </div>;
 }
 
-const NewsBannerWithSkeleton = withSkeleton(NewsBanner, 'banner', 1)
-
-export default NewsBannerWithSkeleton;
+export default NewsBanner;
