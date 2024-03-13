08af9a6bc4f1733f08c23bec9b662221b207d2af
o1afff <oafthree19@gmail.com>
2024-03-13 21:22:40 +0200

    React Context

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,14 @@
 import styles from './styles.module.css'
-const Search = ({keywords, setKeywords}) => {
+import {useTheme} from "../../context/ThemeContext.tsx";
+
+interface Props {
+    keywords: string;
+    setKeywords: (keywords: string) => void;
+}
+const Search = ({ keywords, setKeywords }: Props) => {
+    const { isDark } = useTheme();
     return (
-        <div className={styles.search}>
+        <div className={`${styles.search} ${isDark ? styles.dark : styles.light}`}>
             <input
                 onChange={(e) => setKeywords(e.target.value)}
                 type="text"
